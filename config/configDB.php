a62e0c89d3902888a95b895b1e5f538701018c1e
Rodrigmen <roblesminambresrodrigo@gmail.com>
2021-01-15 08:51:31 +0100

    Add master

--- config/configDB.php
+++ config/configDB.php
@@ -4,7 +4,7 @@
     define('DNS', 'mysql:host=192.168.1.215;dbname=DAW215DBProyectoTema5');//Dirección IP y nombre de la base de datos
     define('USER', 'usuarioDAW215DBProyectoTema5');//Nombre de usuario de la base de datos
     define('PASSWORD', 'P@ssw0rd');//Contraseña del usuario de la base de datos
-    */
+    
 
 
     //CONFIGURACIÓN ENTORNO DE DESARROLLO
@@ -13,11 +13,10 @@
     define('USER', 'usuarioDAW218DBProyectoTema5');//Nombre de usuario de la base de datos
     define('PASSWORD', 'P@ssw0rd');//Contraseña del usuario de la base de datos
    
-
+*/
     //CONFIGURACIÓN 1&1
-   /*
-    define('DNS', 'mysql:host=db5000278682.hosting-data.io;dbname=dbs272026');//Dirección IP y nombre de la base de datos
-    define('USER', 'dbu287777');//Nombre de usuario de la base de datos
-    define('PASSWORD', 'Covid1234$');//Contraseña del usuario de la base de datos
-    */
+   
+    define('DNS', 'mysql:host=db5001094469.hosting-data.io;dbname=dbs939491');//Dirección IP y nombre de la base de datos
+    define('USER', 'dbu1120028');//Nombre de usuario de la base de datos
+    define('PASSWORD', 'Covid1234$');//Contraseña del usuario de la base de datos   
 ?>
