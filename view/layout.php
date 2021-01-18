db216f070baf9e60736b90160b57b5bfa3426f1f
Rodrigmen <roblesminambresrodrigo@gmail.com>
2021-01-18 13:50:20 +0100

    In progress....

--- view/layout.php
+++ view/layout.php
@@ -1,22 +1,26 @@
 <!DOCTYPE html>
-    <html>
-        <head>
-            <meta charset="UTF-8">
-            <title>LoginLogoffMulticapaPOO</title>
-            <meta name="viewport"   content="width=device-width, initial-scale=1.0">
-            <meta name="author"     content="Javier Nieto Lorenzo">
-            <meta name="robots"     content="index, follow">      
-            <link rel="stylesheet"  href="webroot/css/estilos.css"       type="text/css" >
-            <link rel="icon"        href="webroot/media/favicon.ico"    type="image/x-icon">
-        </head>
-        <body>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>LoginLogoffMulticapaPOO</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet"  href="webroot/css/estilos.css"       type="text/css" >
+        <link rel="icon" type="image/jpg" href="../webroot/css/images/favicon.jpg"/>
+    </head>
+    <body>
 
-            <?php require_once $vista ?>
+        <?php require_once $vista ?>
 
-        <footer class="fixed">
-            <a href="http://daw217.ieslossauces.es/" target="_blank"> <img src="webroot/media/oneandone.png" alt="oneandone icon" width="40"></a>
-            <address>  <a href="../../index.html">&copy; 2020-2021 Javier Nieto Lorenzo</a> </address>
-            <a href="https://github.com/JavierNLSauces/" target="_blank"><img class="github" width="40" src="webroot/media/github.png" ></a>
+        <footer>
+            <ul>
+                <li>&copy2020-2021 | Rodrigo Robles Miñambres</li>
+                <li>
+                    <a target="_blank" href="https://github.com/Rodrigmen/Tema6LoginLogoff">
+                        <img id="imggit" title="GitHub" src="webroot/css/img/github.png"  alt="GITHUB">
+                    </a>
+                </li>
+            </ul>            
         </footer>
     </body>
 </html>
--- view/vLogin.php
+++ view/vLogin.php
@@ -1,20 +1,26 @@
 <header>
-    <h1>Login</h1>
+    <a href="../indexProyectoDWES.php">
+        <img class="imgprinc" src="webroot/css/img/flechaatras.png" alt="Atrás" title="Atrás"/>
+    </a>
+    <a href="../../../../index.html">
+        <img class="imgprinc" id="casa" src="webroot/css/img/inicio.png" alt="Página Principal" title="Página Principal"/>
+    </a>
+    <h1 id="titulo">Inicio de sesión</h1>
 </header>
-<main class="flex-container-align-item-center">
-    <form name="formularioIdioma" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+<main>
+    <form id="cookies" name="formularioIdioma" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
         <button <?php echo ($_COOKIE['idioma'] == "es") ? "style='color: black;'" : null; ?> class="idioma " type="submit" name="idiomaElegido" value="es"> Castellano</button>
         <button <?php echo ($_COOKIE['idioma'] == "en") ? "style='color: black;'" : null; ?> class="idioma" type="submit" name="idiomaElegido" value="en"> English</button>
     </form>
-    <form name="login" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+    <form class="enter" name="login" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
 
-        <div>
+        <div class="required">
             <label for="CodUsuario"><?php echo $aLang[$_COOKIE['idioma']]['user']; ?></label>
             <input class="required" type="text" id="CodUsuario" name="CodUsuario" placeholder="<?php echo $aLang[$_COOKIE['idioma']]['user']; ?>" value="<?php
             echo (isset($_REQUEST['CodUsuario'])) ? $_REQUEST['CodUsuario'] : null;
             ?>">
         </div>
-        <div>
+        <div class="required">
             <label for="Password"><?php echo $aLang[$_COOKIE['idioma']]['password']; ?></label>
             <input class="required" type="password" id="Password" name="Password" value="<?php
             echo (isset($_REQUEST['Password'])) ? $_REQUEST['Password'] : null;
@@ -22,10 +28,7 @@
         </div>
 
         <div>
-            <button class="button" type="submit" name="IniciarSesion"><?php echo $aLang[$_COOKIE['idioma']]['login']; ?></button>
-            <!--
-            <a class="registrarse" href="registro.php"><?php echo $aLang[$_COOKIE['idioma']]['signup']; ?></a>
-            -->
+            <button class="button" type="submit" name="IniciarSesion"><?php echo $aLang[$_COOKIE['idioma']]['login']; ?></button>           
         </div>
 
     </form>
